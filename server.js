d629470243b62deb29dec9582a37c453c841a93e
Derick Cardin <derickc50@gmail.com>
2018-10-23 02:33:08 +0000

    added get route to display my name

--- server.js
+++ server.js
@@ -4,4 +4,10 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.get ('/', (request, response) => response.send('<h1>Derick Cardin</h1>'));
+
+app.get('/', (request, response) => response.send("<h1>Derick Cardin</h1>"))
+
+
+app.listen(process.env.PORT, process.env.IP, 8080, function() {
+    console.log('Listening on port 8080');
+});
