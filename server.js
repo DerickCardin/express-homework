1b051d46cb941e6560e9ca34aeafe3ad03066b1e
Derick Cardin <derickc50@gmail.com>
2018-10-23 02:40:16 +0000

    Get route for name and string

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ app.use(bodyParser.json());
 
 app.get('/', (request, response) => response.send("<h1>Derick Cardin</h1>"))
 
+app.get('/user/:name', function(req, res) {
+    res.send('Welcome ' + req.params.name + '!');
+});
 
 app.listen(process.env.PORT, process.env.IP, 8080, function() {
     console.log('Listening on port 8080');
